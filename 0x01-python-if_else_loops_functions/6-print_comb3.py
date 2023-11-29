4d80678f81efb2a41738d19036f2096fb74548dd
KelvinChester <kenechukwuabuonovo@gmail.com>
2023-11-28 19:31:29 -0800

    test 1

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for tens_digit in range(9):
+    for ones_digit in range(tens_digit + 1, 10):
+        if tens_digit == 8 and ones_digit == 9:
+            print("{:02d}".format(tens_digit * 10 + ones_digit))
+        else:
+            print("{:02d}, ".format(tens_digit * 10 + ones_digit), end="")
