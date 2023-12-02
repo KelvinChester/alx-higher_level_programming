c1c96f25a0747963e10e7900e5042d05df44188f
KelvinChester <kenechukwuabuonovo@gmail.com>
2023-12-01 20:10:30 -0800

    test4

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    calc_argument = argv
+    length_argument = len(calc_argument) - 1 
+if length_argument == 0:
+    print("Usage: {} <a> <operator> <b>".format(calc_argument[0]))
+    exit(1)
+ops = {'+':add, '-':sub, '*':mul, '/':div}
+if calc_argument[2] in ops:
+    num1 = int(calc_argument[1])
+    num2 = int(calc_argument[3])
+    op = ops[calc_argument[2]]
+    result = op(num1, num2)
+    print("{:d} {:s} {:d} = {:d}".format(num1, calc_argument[2], num2, result))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+exit(0)
